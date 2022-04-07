a438774a4e3a6ffb354678732949dd81e9e01aed
salahdin <salahdinjemal@gmail.com>
2022-04-07 18:29:30 +0300

    modify models

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,9 @@ from core.models import Company
 
 
 class AdminProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    company = models.ManyToManyField(Company, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+    manages = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="manager")
     img = models.ImageField(upload_to='core/avatar', blank=True, default='core/avatar/blank_profile.png')
 
     def __str__(self):
-        return (str(self.user))
+        return str(self.user)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 
 
 @login_required(login_url='/accounts/login/')
