c63150e7c8d05f07506ac971176112ff421cbb9a
salahdin <salahdinjemal@gmail.com>
2022-05-30 12:33:57 +0300

    initial commit

--- AirtimeManagmentSystem/settings.py
+++ AirtimeManagmentSystem/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-(kp!6q$-bajqkiaad1p+&tao-6gz39%nuy&ew*q#ak8u&ahvke
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['airtime-management.herokuapp.com', '127.0.0.1:8000']
 
 # initialize environment variables
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,10 +109,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join('static'),)
 
+MEDIAFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -119,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SMS_API = {
     'server':  env('SERVER'),
-    'api_key': env('API_KEY')
+    'api_key': env('API_KEY'),
 }
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AirtimeManagmentSystem.wsgi --log-file -
