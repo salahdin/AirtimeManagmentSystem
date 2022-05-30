726083ef2c8653448c76feca56bfc19b20bada6b
salahdin <salahdinjemal@gmail.com>
2022-05-30 19:03:44 +0300

    configure DB

--- AirtimeManagmentSystem/settings.py
+++ AirtimeManagmentSystem/settings.py
@@ -77,6 +77,21 @@ DATABASES = {
     }
 }
 
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if ON_HEROKU:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
